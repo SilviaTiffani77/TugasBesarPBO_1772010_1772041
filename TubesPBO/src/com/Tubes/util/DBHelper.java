868d5f56525272e8b897d4f134355601c6dd4dc1
SilviaTiffani77 <SilviaTiffani77@gmail.com>
2020-01-08 15:25:00 +0700

    DBHelper

--- TubesPBO/src/com/Tubes/util/DBHelper.java
+++ TubesPBO/src/com/Tubes/util/DBHelper.java
@@ -1,18 +1,20 @@
 package com.Tubes.util;
 
+//Silvia Tiffani 1772010
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBHelper {
 
-    private static final String URL="jdbc:mysql://localhost:3306/pbo2w04";
-    private static final String USERNAME ="root";
-    private static final String PASSWORD="";
+    private static final String URL = "jdbc:mysql://localhost:3306/TubesPBO";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
 
     public static Connection createMySQLConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection connection= DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         connection.setAutoCommit(false);
         return connection;
     }
